38c7ec7f6db9e27696dae481601ec5389674b60b
GCPIYUMAL <g.c.piyumal98@gmail.com>
2023-05-06 14:06:54 +0530

    Updated

---
+++ app/src/main/java/com/example/busticket/seat_select.kt
@@ -0,0 +1,11 @@
+package com.example.busticket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class seat_select : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seat_select)
+    }
+}
