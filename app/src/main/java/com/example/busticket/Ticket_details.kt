11f5a32ae92a6b3ed65078824e4be4fa5c25b82c
GCPIYUMAL <g.c.piyumal98@gmail.com>
2023-05-10 20:07:23 +0530

    change seat activity

---
+++ app/src/main/java/com/example/busticket/Ticket_details.kt
@@ -0,0 +1,11 @@
+package com.example.busticket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ticket_details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket_details)
+    }
+}
---
+++ app/src/main/java/com/example/busticket/payment_method.kt
@@ -0,0 +1,11 @@
+package com.example.busticket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class payment_method : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_method)
+    }
+}
